63e2a4f6bc6843fd3ef923a4dfb8f82645a55808
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2024-11-21 23:19:40 +0200

    prepare view.py functions

--- _python_project_mypms/admin_mypms/models.py
+++ _python_project_mypms/admin_mypms/models.py
@@ -46,6 +46,16 @@ class ProjectManager(models.Manager):
 ###########################
 ###### Table Classes ######
 ###########################
+class Client(models.Model):
+    company_name = models.CharField(max_length=255)
+    contact_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = ClientManager()
+
 class Department(models.Model):
     name = models.CharField(max_length=255)
     desc = models.TextField( default="No desription added!")
@@ -58,16 +68,6 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
-
-class Client(models.Model):
-    company_name = models.CharField(max_length=255)
-    contact_name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = ClientManager()
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
@@ -83,6 +83,34 @@ class Project(models.Model):
 #################################
 ###### Methods (Functions) ######
 #################################
+
+### get all metheods:
+def get_all_clients():
+    return Client.objects.all()
+
+def get_all_departments():
+    return Department.objects.all()
+
+def get_all_users():
+    return User.objects.all()
+
+def get_all_projects():
+    return Project.objects.all()
+
+### create methods:
+def create_client(data):
+    Client.objects.create(
+        company_name = data['company_name'],
+        contact_name = data['contact_name'],
+        address = data['address'],
+        email = data['email']
+    )
+
+def create_department(data):
+    Department.objects.create(
+        name = data['name']
+    )
+   
 def create_user(data):
     password =data['password']
     hashed_pwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
@@ -90,12 +118,24 @@ def create_user(data):
         username = data['username'],
         name = data['name'],
         password = hashed_pwd,
-        department = data['department']
+        department = Department.objects.get(id=data['department_id'])
     )
 
+def create_project(data):
+    Project.objects.create(
+        name = data['name'],
+        client = Client.objects.get(id=data['client_id']),
+        location = data['location'],
+        main_contractor = data['main_contractor'],
+        manager = User.objects.get(id=data['manager_id']),
+        deadline = data['deadline']
+    )
+
+# get special methods:
 def get_user_by_username(username):
     return User.objects.filter(username=username)
 
+#check methods:
 def is_user_exist(data):
     users = get_user_by_username(username = data['username'])
     if len(users) == 0:
--- _python_project_mypms/admin_mypms/urls.py
+++ _python_project_mypms/admin_mypms/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
-    path('adminclient',views.adminclient),
-    path('AdminDashBoard',views.AdminDashBoard),
-    path('adminProject',views.adminProject),
-    path('adminUsers',views.adminUsers),
-    path('projectmanangerdashboard',views.projectmanangerdashboard),
-    path('reports',views.reports),
-    path('userdashboard',views.userdashboard)
-
+    path('admin_dashboard',views.admin_dashboard),
+    path('view_clients',views.view_clients),
+    path('view_users',views.view_users),
+    path('view_projects',views.view_projects),
+    path('project_manager_dashboard',views.project_manager_dashboard),
+    path('report',views.report),
+    path('user_dashboard',views.user_dashboard),
+    path('check_login',views.check_login)
 ]
