21457b4516034def4056219effe211ac339d7568
jalalman <jalal.i.hemmo@gmail.com>
2024-11-21 10:00:55 +0200

    templates

--- _python_project_mypms/admin_mypms/urls.py
+++ _python_project_mypms/admin_mypms/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.login),
     path('adminclient',views.adminclient),
-    path('AdminDashBoard',views.AdminDashBoard)
+    path('AdminDashBoard',views.AdminDashBoard),
+    path('adminProject',views.adminProject),
+    path('adminUsers',views.adminUsers),
+    path('projectmanangerdashboard',views.projectmanangerdashboard),
+    path('reports',views.reports),
+    path('userdashboard',views.userdashboard)
+
 ]
