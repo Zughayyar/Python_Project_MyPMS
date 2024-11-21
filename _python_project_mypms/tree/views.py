bcdbea69cdfe43e7ef1a5bb41c37f22d03dfaeb0
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2024-11-21 12:15:18 +0200

    add and modify tree table

--- _python_project_mypms/tree/views.py
+++ _python_project_mypms/tree/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import HttpResponse ,render
+from django.shortcuts import HttpResponse ,render, redirect
+from . import models
 
 # Create your views here.
 def createtree(request):
-    return render(request,"createtree.html")
+    context = {
+        'elements' : models.get_all_elements(),
+        'subelements' : models.get_all_sub_elements(),
+        'projects' : models.get_all_projects()
+    }
+    return render(request,"createtree.html", context)
+
+def addelement(request):
+    if request.method == "POST":
+        models.add_element(request.POST)
+        return redirect('/tree/createtree')
+    else:
+        return HttpResponse("something went wrong!!")
+
+def addsubelement(request):
+    if request.method == "POST":
+        models.add_sub_element(request.POST)
+        return redirect('/tree/createtree')
+    else:
+        return HttpResponse("something went wrong!!")
